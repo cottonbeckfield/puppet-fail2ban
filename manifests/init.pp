1d3e9a80f8e3ad8fe01345e8d23c7fb4aa3ae8ef
Courtney C <courtneycotton@berkeley.edu>
2014-10-08 12:05:47 -0700

    Removed old cold.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,55 +12,48 @@
 # (Required) E-mail used in template to send alerts to on behalf of a Jail.
 #
 
-class fail2ban(
+class fail2ban (
   $teamEmail = hiera('team_email', ''),
 ){
   package { 'fail2ban':
     ensure => installed,
-    before => [ File['/etc/fail2ban/fail2ban.conf'],
-                File['/etc/fail2ban/jail.conf'],
-                File['/etc/fail2ban/jail.local'],
-                File['/etc/fail2ban/action.d/sendmail-whois.conf'],
-                File['/etc/fail2ban/filter.d/sshd.conf'],
-              ],
-  }
-
+    #before => [ File['/etc/fail2ban/fail2ban.conf'], 
+    #            File['/etc/fail2ban/jail.conf'],
+    #            File['/etc/fail2ban/jail.local'],
+    #	        File['/etc/fail2ban/action.d/sendmail-whois.conf'], 
+    #		File['/etc/fail2ban/filter.d/sshd.conf'],
+    #          ],
+  } ->
   file { '/etc/fail2ban/fail2ban.conf':
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template('app_fail2ban/fail2ban.erb'),
-  }
-
+    content => template('fail2ban/fail2ban.erb'),
+  } ->
   file { '/etc/fail2ban/jail.conf':
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template('app_fail2ban/jail.erb'),
-  }
-
-
+    content => template('fail2ban/jail.erb'),
+  } ->
   file { '/etc/fail2ban/jail.local':
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template('app_fail2ban/jail.local.erb'),
-  }
-
+    content => template('fail2ban/jail.local.erb'),
+  } ->
   file { '/etc/fail2ban/action.d/sendmail-whois.conf':
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template('app_fail2ban/sendmail-whois.erb'),
-  }
-
+    content => template('fail2ban/sendmail-whois.erb'),
+  } ->
   file { '/etc/fail2ban/filter.d/sshd.conf':
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template('app_fail2ban/sshd.erb'),
-  }
-
+    content => template('fail2ban/sshd.erb'),
+  } ~>
   service { 'fail2ban':
     ensure  => running,
     require => Package['fail2ban'],
